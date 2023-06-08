0c979ceb39c7d7908555ae5bdc806e0bb8691c99
LongHoangNguyen <longnh14794@gmail.com>
2023-06-08 14:08:48 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# azure-devops
+This is repo for azure devops
+
+## This was edited in Cloud Shell
---
+++ hello.py
@@ -0,0 +1,6 @@
+def toyou(x):
+    return "hello %s" % x
+def add(x):
+    return x + 1
+def subtract(x):
+    return x - 1
