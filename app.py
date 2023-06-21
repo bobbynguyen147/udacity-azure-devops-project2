68079ccad7d02784d0c37836e1a38eea36d949f5
LongHoangNguyen <longnh14794@gmail.com>
2023-06-21 15:06:29 +0700

    check

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home APP</h3>"
+    html = "<h3>Sklearn Prediction Home APP - Long Edit</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
